3080816eaec944a69c7afe03dc647c2e7a69c4a6
raminrayhani <rayhani.ramin@gmail.com>
2017-07-13 20:22:05 -0400

    First commit

--- config/connection.js
+++ config/connection.js
@@ -12,9 +12,9 @@ var source = {
 	},
 	jawsDB: {
 		port: 3306,
-		host: 'erxv1bzckceve5lh.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-		user: 'wpvdcpcojry0le49',
-		password: 'wps9xl2h4fosvhgn',
+		host: 'gx97kbnhgjzh3efb.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+		user: 'vt0eotyupp1i15ni',
+		password: 'qz6dygedxs71a8ou',
 		database: 'u4wogneutc6rikrq'
 	}
 
